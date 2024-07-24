02be689d1fb0e9e858395fedc4c879cd54d4a33e
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-07-24 00:27:12 -0300

    create style for userLibrary page

--- src/pages/userLibrary.tsx
+++ src/pages/userLibrary.tsx
@@ -1,49 +1,208 @@
 // import { useState } from 'react'
-import Navbar from '../components/navBar'
+import { CiPause1, CiSearch } from 'react-icons/ci'
 import SideBar from '../components/sideBar'
-
-// import StatusFinished from '../../components/GamesByStatus/StatusFinished'
-// import StatusPlaying from '../../components/GamesByStatus/StatusPlaying'
-// import StatusPaused from '../../components/GamesByStatus/StatusPaused'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { UserGame } from '../types'
+import { Cookies } from 'typescript-cookie'
+import { FaFlagCheckered } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
+import { PiGameControllerBold } from 'react-icons/pi'
 
 export default function UserLibrary() {
-  // const [userGames, setUserGames] = useState([])
+  // const [currentGame, setCurrentGame] = useState<UserGame | null>(null)
+  // const [open, setOpen] = useState(false)
 
-  // const playingGames = userGames
-  //   .filter(game => game?.gameStatus?.status === 'playing')
-  //   .slice(0, 5)
+  const [userGames, setUserGames] = useState<UserGame[]>([])
 
-  // const playing = userGames?.filter(
-  //   game => game?.gameStatus?.status === 'playing'
-  // )
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
 
-  // const finishedGames = userGames
-  //   ?.filter(game => game?.gameStatus?.status === 'finished')
-  //   .slice(0, 5)
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+
+  const userId = localStorage.getItem('userId')
+
+  useEffect(() => {
+    const url = new URL(`http://localhost:3333/users/${userId}/userGames`)
+    url.searchParams.set('pageIndex', String(page - 1))
+
+    if (search.length > 0) {
+      url.searchParams.set('query', search)
+    }
+
+    fetch(url, {
+      headers: { Authorization: `Bearer ${Cookies.get('token')}` }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setUserGames(data.userGames)
+      })
+  }, [page, search, userId])
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, '', url)
+    setPage(page)
+  }
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, '', url)
+
+    setSearch(search)
+  }
+
+  function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
+  }
 
   // const finished = userGames?.filter(
   //   game => game?.gameStatus?.status === 'finished'
   // )
 
-  // const pausedGames = userGames
-  //   ?.filter(game => game?.gameStatus?.status === 'paused')
-  //   .slice(0, 5)
+  const pausedGames = userGames
+    .filter(
+      UserGameStatus => UserGameStatus.UserGamesStatus.status === 'paused'
+    )
+    .slice(0, 2)
 
-  // const paused = userGames?.filter(
-  //   game => game?.gameStatus?.status === 'paused'
-  // )
+  const finishedGames = userGames
+    .filter(
+      UserGameStatus => UserGameStatus.UserGamesStatus.status === 'finished'
+    )
+    .slice(0, 2)
+
+  const playingGames = userGames
+    .filter(
+      UserGameStatus => UserGameStatus.UserGamesStatus.status === 'playing'
+    )
+    .slice(0, 2)
+  console.log(playingGames)
 
   return (
     <>
-      <div className="flex flex-row w-full min-h-dvh bg-[#1A1C26] ">
+      <div className="flex flex-col w-full min-h-dvh bg-[#1A1C26] ">
         <SideBar />
 
-        <Navbar />
+        <div className="flex gap-3 items-center">
+          <div className=" left-[20rem] top-[2rem] w-[30rem] mx-44 mt-10">
+            <CiSearch className="size-6 text-[#8F8F8F] absolute top-[3.625rem] left-[12.5rem]" />
+            <input
+              onChange={onSearchInputChange}
+              value={search}
+              className="bg-[#272932] text-[#8F8F8F] rounded-2xl block w-full h-[62px] pl-[62px]"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
 
-        <div className=" flex flex-col place-items-stretch mt-40 mx-64 gap-4 2xl:h-full xl:h-full lg:h-full md:h-full">
-          {/* <StatusPlaying playing={playing} playingGames={playingGames} />
-          <StatusFinished finished={finished} finishedGames={finishedGames} />
-          <StatusPaused paused={paused} pausedGames={pausedGames} /> */}
+        <div className="flex flex-col mt-14 mx-60">
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-row gap-2 items-center ">
+              <h1 className="text-2xl font-bold text-white">Playing</h1>
+              <div className="flex flex-row items-center gap-1">
+                <PiGameControllerBold className="size-4 text-white" />
+                <p className="text-sm text-white font-bold">
+                  {playingGames?.length}
+                </p>
+              </div>
+            </div>
+            <div>
+              <Link
+                to="/library/finishedGames"
+                className="text-[#8F8F8F]"
+                state={playingGames}
+              >
+                Mostrar tudo
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4">
+            {playingGames &&
+              playingGames.map(({ game }) => (
+                <div key={game.id}>
+                  <img src={game.gameBanner} alt={game.gameName} />
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="flex flex-col mt-14 mx-60">
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-row gap-2 items-center ">
+              <h1 className="text-2xl font-bold text-white">Finished</h1>
+              <div className="flex flex-row items-center gap-1">
+                <FaFlagCheckered className="size-4 text-white" />
+                <p className="text-sm text-white">{finishedGames?.length}</p>
+              </div>
+            </div>
+            <div>
+              <Link
+                to="/library/finishedGames"
+                className="text-[#8F8F8F]"
+                state={finishedGames}
+              >
+                Mostrar tudo
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4">
+            {finishedGames &&
+              finishedGames.map(({ game }) => (
+                <div key={game.id}>
+                  <img src={game.gameBanner} alt={game.gameName} />
+                </div>
+              ))}
+          </div>
+        </div>
+        <div className="flex flex-col mt-14 mx-60">
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-row gap-2 items-center ">
+              <h1 className="text-2xl font-bold text-white">Paused</h1>
+              <div className="flex flex-row items-center gap-1">
+                <CiPause1 className="size-4 text-white" />
+                <p className="text-sm text-white">{pausedGames?.length}</p>
+              </div>
+            </div>
+            <div>
+              <Link
+                to="/library/finishedGames"
+                className="text-[#8F8F8F]"
+                state={pausedGames}
+              >
+                Mostrar tudo
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row gap-4">
+            {pausedGames &&
+              pausedGames.map(({ game }) => (
+                <div key={game.id}>
+                  <img src={game.gameBanner} alt={game.gameName} />
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </>
