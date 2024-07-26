485d9f90039994ea2edd3a5810e9bf0cb3cf2d03
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-07-25 23:45:21 -0300

    fix style user game modal

--- src/components/userGamesComponents/userGameModal.tsx
+++ src/components/userGamesComponents/userGameModal.tsx
@@ -15,8 +15,8 @@ export default function UserGameModal({
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-black/70 inset-0 fixed" />
-        <Dialog.Content className="fixed bg-[#272932] pt-4 px-12 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg ">
+        <Dialog.Overlay className="bg-black/50 inset-0 fixed" />
+        <Dialog.Content className="fixed bg-[#272932] pt-4 px-12 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
           <div className="flex items-center justify-between">
             <Dialog.Title className="text-2xl"></Dialog.Title>
             <Dialog.Description></Dialog.Description>
--- src/components/userGamesComponents/userGamesForm.tsx
+++ src/components/userGamesComponents/userGamesForm.tsx
@@ -10,6 +10,9 @@ import { FiPause } from 'react-icons/fi'
 import { RxCross1, RxRowSpacing } from 'react-icons/rx'
 import { Cookies } from 'typescript-cookie'
 import UserGameModal from './userGameModal'
+import { SlOptionsVertical } from 'react-icons/sl'
+import { Button } from '@radix-ui/themes'
+import { removeGame } from '../../services/userServices'
 
 export function UserGamesForm({
   afterSave,
@@ -57,23 +60,29 @@ export function UserGamesForm({
     afterSave()
   }
 
+  async function handleSubmitRemoveGame() {
+    const data = { gameId: game?.id }
+    console.log(data)
+    await removeGame(data)
+  }
+
   if (game === null) {
     return null
   }
 
   return (
     <form onSubmit={handleSubmit} key={gameStatus?.id} className="">
-      <fieldset disabled={saving} className="group">
-        <div className="group-disabled:opacity-50 flex w-[649px] h-[430px]">
+      <fieldset disabled={saving} className="group ">
+        <div className="group-disabled:opacity-50 flex  min-w-[420px] min-h-[430px]">
           <img
-            className="-ml-12 -mt-4 rounded-l-lg"
+            className="-ml-12 -mt-4 rounded-l-lg w-80"
             src={game?.gameBanner}
             alt=""
           />
           <div className="flex flex-col gap-1 items-center">
             <div className="flex flex-col items-center mt-2">
               <div className="ml-10 ">
-                <h2 className="text-3xl font-bold">{game.gameName}</h2>
+                <h2 className="text-2xl font-bold">{game.gameName}</h2>
               </div>
               <div className="mt-2 relative">
                 <Collapsible.Root
@@ -150,7 +159,7 @@ export function UserGamesForm({
               <div className="flex flex-row gap-2">
                 {game?.categories.map((category, index) => (
                   <span
-                    className="bg-black rounded-lg text-white font-semibold text-lg flex justify-center items-center h-8 w-28"
+                    className={`bg-black rounded-lg text-white font-semibold text-lg flex justify-center items-center p-2`}
                     key={index}
                   >
                     {category.categoryName}
@@ -164,75 +173,112 @@ export function UserGamesForm({
               <p className="text-xl font-bold">{game?.gameStudio.studioName}</p>
             </div>
 
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger asChild>
-                <button
-                  className="rounded-md w-40 h-10 gap-4 flex flex-row items-center justify-center text-white bg-[#6930CD] shadow-[0_2px_10px] shadow-blackA4 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet11 ml-8 text-lg font-medium"
-                  aria-label="Customise options"
-                >
-                  {selectedStatus === '1' && (
-                    <PiFlagCheckeredBold className="size-4" />
-                  )}
-                  {selectedStatus === '2' && (
-                    <IoMdHourglass className="size-4" />
-                  )}
-                  {selectedStatus === '3' && <FiPause className="size-4" />}
-                  <p>
+            <div className="flex flex-row gap-2 ml-6 my-auto">
+              <h3 className="text-xl">Publisher:</h3>
+              <p className="text-xl font-bold">
+                {game?.publisher.publisherName}
+              </p>
+            </div>
+            <div className="flex gap-2 mt-4">
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                  <button
+                    className="rounded-md w-40 h-10 gap-4 flex flex-row items-center justify-center text-white bg-[#6930CD] shadow-[0_2px_10px] shadow-blackA4 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet11 ml-8 text-lg font-medium"
+                    aria-label="Customise options"
+                  >
+                    {selectedStatus === '1' && (
+                      <PiFlagCheckeredBold className="size-4" />
+                    )}
+                    {selectedStatus === '2' && (
+                      <IoMdHourglass className="size-4" />
+                    )}
+                    {selectedStatus === '3' && <FiPause className="size-4" />}
+                    <p>
+                      {selectedStatus === '1'
+                        ? 'Finished'
+                        : selectedStatus === '2'
+                        ? 'Playing'
+                        : 'Paused'}
+                    </p>
+                  </button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Portal>
+                  <DropdownMenu.Content
+                    className="w-36  bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade "
+                    sideOffset={5}
+                  >
+                    <DropdownMenu.RadioGroup
+                      value={selectedStatus?.toString()}
+                      onValueChange={handleStatusChange}
+                    >
+                      <DropdownMenu.RadioItem
+                        className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                        value="1"
+                      >
+                        <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
+                          <PiFlagCheckeredBold />
+                        </DropdownMenu.ItemIndicator>
+                        Finished
+                      </DropdownMenu.RadioItem>
+                      <DropdownMenu.RadioItem
+                        className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                        value="2"
+                      >
+                        <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
+                          <IoMdHourglass />
+                        </DropdownMenu.ItemIndicator>
+                        Playing
+                      </DropdownMenu.RadioItem>
+                      <DropdownMenu.RadioItem
+                        className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                        value="3"
+                      >
+                        <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
+                          <FiPause />
+                        </DropdownMenu.ItemIndicator>
+                        Paused
+                      </DropdownMenu.RadioItem>
+                    </DropdownMenu.RadioGroup>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Root>
+
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger className="rounded-md w-12 h-10 inline-flex items-center justify-center bg-[#6930CD]  outline-none ">
+                  <SlOptionsVertical />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content className="bg-[#6930CD] rounded-md p-2 m-1  will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade hover:bg-[#2214CD]">
+                  <Button
+                    className="ps-3 group text-[13px] leading-none text-white rounded-sm flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-slate-700 data-[highlighted]:text-violet1 gap-1 "
+                    onClick={() => {
+                      handleSubmitRemoveGame()
+                    }}
+                  >
+                    Remover de{' '}
+                    {selectedStatus === '1' && (
+                      <PiFlagCheckeredBold className="size-4" />
+                    )}
+                    {selectedStatus === '2' && (
+                      <IoMdHourglass className="size-4" />
+                    )}
+                    {selectedStatus === '3' && <FiPause className="size-4" />}
                     {selectedStatus === '1'
                       ? 'Finished'
                       : selectedStatus === '2'
                       ? 'Playing'
                       : 'Paused'}
-                  </p>
-                </button>
-              </DropdownMenu.Trigger>
-
-              <DropdownMenu.Portal>
-                <DropdownMenu.Content
-                  className="w-36  bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade "
-                  sideOffset={5}
-                >
-                  <DropdownMenu.RadioGroup
-                    value={selectedStatus?.toString()}
-                    onValueChange={handleStatusChange}
-                  >
-                    <DropdownMenu.RadioItem
-                      className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-                      value="1"
-                    >
-                      <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
-                        <PiFlagCheckeredBold />
-                      </DropdownMenu.ItemIndicator>
-                      Finished
-                    </DropdownMenu.RadioItem>
-                    <DropdownMenu.RadioItem
-                      className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-                      value="2"
-                    >
-                      <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
-                        <IoMdHourglass />
-                      </DropdownMenu.ItemIndicator>
-                      Playing
-                    </DropdownMenu.RadioItem>
-                    <DropdownMenu.RadioItem
-                      className="text-base leading-none text-violet11 rounded-[3px] flex items-center justify-center h-[25px] px-[5px] relative select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-                      value="3"
-                    >
-                      <DropdownMenu.ItemIndicator className="absolute left-3 w-[25px] inline-flex items-center justify-center">
-                        <FiPause />
-                      </DropdownMenu.ItemIndicator>
-                      Paused
-                    </DropdownMenu.RadioItem>
-                  </DropdownMenu.RadioGroup>
+                  </Button>
+                  <DropdownMenu.Arrow className="fill-black" />
                 </DropdownMenu.Content>
-              </DropdownMenu.Portal>
-            </DropdownMenu.Root>
-            <div className="my-auto pb-2 ml-56 flex gap-4">
+              </DropdownMenu.Root>
+            </div>
+
+            <div className="my-auto pb-2 flex gap-4 ml-72 mt-12">
               <UserGameModal.Close className="rounded px-4 text-sm font-medium text-gray-500 hover:text-gray-600">
                 Cancel
               </UserGameModal.Close>
               <button className="inline-flex items-center justify-center rounded bg-[#6930CD] px-4 py-2 text-sm font-medium text-white hover:bg-green-600 group-disabled:pointer-events-none">
-                {/* <Spinner className="absolute h-4 group-enabled:opacity-0" /> */}
                 <span className="group-disabled:opacity-0">Save</span>
               </button>
             </div>
