86c0661085fbeeccd8614c69539b1055a49627c0
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-08-06 14:14:53 -0300

    some changes on style in userLibrary

--- src/pages/userLibrary.tsx
+++ src/pages/userLibrary.tsx
@@ -1,12 +1,13 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import { CiSearch } from 'react-icons/ci'
+import { useEffect, useState } from 'react'
 import { PiGameControllerBold } from 'react-icons/pi'
 import { Link } from 'react-router-dom'
 import { Cookies } from 'typescript-cookie'
 import SideBar from '../components/sideBar'
-import UserGameModal from '../components/userGamesComponents/userGameModal'
+import { UserGameModal } from '../components/userGamesComponents/userGameModal'
 import { UserGamesForm } from '../components/userGamesComponents/userGamesForm'
 import { Game, UserGame } from '../types'
+
+import userProfilePictureDefault from '../assets/Default_pfp.svg.png'
 
 export function UserLibrary() {
   const [currentGame, setCurrentGame] = useState<Game | null>(null)
@@ -17,35 +18,10 @@ export function UserLibrary() {
 
   const [userGames, setUserGames] = useState<UserGame[]>([])
 
-  const [page, setPage] = useState(() => {
-    const url = new URL(window.location.toString())
-
-    if (url.searchParams.has('page')) {
-      return Number(url.searchParams.get('page'))
-    }
-
-    return 1
-  })
-
-  const [search, setSearch] = useState(() => {
-    const url = new URL(window.location.toString())
-
-    if (url.searchParams.has('search')) {
-      return url.searchParams.get('search') ?? ''
-    }
-
-    return ''
-  })
-
   const userId = localStorage.getItem('userId')
 
   useEffect(() => {
     const url = new URL(`http://localhost:3333/users/${userId}/userGames`)
-    url.searchParams.set('pageIndex', String(page - 1))
-
-    if (search.length > 0) {
-      url.searchParams.set('query', search)
-    }
 
     fetch(url, {
       headers: { Authorization: `Bearer ${Cookies.get('token')}` }
@@ -54,31 +30,7 @@ export function UserLibrary() {
       .then(data => {
         setUserGames(data.userGames)
       })
-  }, [page, search, userId])
-
-  function setCurrentPage(page: number) {
-    const url = new URL(window.location.toString())
-
-    url.searchParams.set('page', String(page))
-
-    window.history.pushState({}, '', url)
-    setPage(page)
-  }
-
-  function setCurrentSearch(search: string) {
-    const url = new URL(window.location.toString())
-
-    url.searchParams.set('search', search)
-
-    window.history.pushState({}, '', url)
-
-    setSearch(search)
-  }
-
-  function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
-    setCurrentSearch(event.target.value)
-    setCurrentPage(1)
-  }
+  }, [userId])
 
   useEffect(() => {
     setPlayingGame(
@@ -139,7 +91,7 @@ export function UserLibrary() {
 
   const playingGames = playingGame.slice(0, 5)
 
-  const finishedGames = finishedGame.slice(0, 5)
+  const finishedGames = finishedGame.slice(0, 6)
 
   const pausedGames = pausedGame.slice(0, 5)
 
@@ -148,7 +100,7 @@ export function UserLibrary() {
       <div className="flex flex-col w-full min-h-dvh bg-[#1A1C26] ">
         <SideBar />
 
-        <div className="flex gap-3 items-center">
+        {/* <div className="flex gap-3 items-center">
           <div className=" left-[20rem] top-[2rem] w-[30rem] mx-44 mt-10">
             <CiSearch className="size-6 text-[#8F8F8F] absolute top-[3.625rem] left-[12.5rem]" />
             <input
@@ -159,164 +111,229 @@ export function UserLibrary() {
               placeholder="Search"
             />
           </div>
-        </div>
-
-        <div className="flex flex-col mt-10 mx-52 bg-[#272932] p-8">
-          <div className="flex flex-row gap-2 justify-between">
-            <div className="flex flex-row gap-2 items-center mb-2">
-              <h1 className="text-3xl font-bold text-white">Finished</h1>
-              <div className="flex flex-row items-center gap-1">
-                <PiGameControllerBold className="size-6 text-white" />
-                <p className="text-base text-white font-bold">
-                  {finishedGame?.length}
-                </p>
+        </div> */}
+        <div className="flex flex-col items-center mt-4">
+          <Link to="/profile" className="">
+            <img
+              src={userProfilePictureDefault}
+              alt=""
+              className="object-fill size-16 rounded-2xl"
+            />
+          </Link>
+          <div className="flex flex-col mt-10 bg-[#272932] p-8">
+            <div className="flex flex-row gap-2 justify-between items-center">
+              <div className="flex flex-row gap-2 mb-2">
+                <h1 className="text-3xl font-bold text-white">Finished</h1>
+                <div className="flex flex-row items-center gap-1">
+                  <PiGameControllerBold className="size-6 text-white" />
+                  <p className="text-base text-white font-bold">
+                    {finishedGame?.length}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <Link
+                  to="/userLibrary/playingGames"
+                  className="text-[#8F8F8F]"
+                  state={finishedGame}
+                >
+                  Show all
+                </Link>
               </div>
             </div>
-            <div>
-              <Link
-                to="/userLibrary/playingGames"
-                className="text-[#8F8F8F]"
-                state={finishedGame}
-              >
-                Mostrar tudo
-              </Link>
-            </div>
-          </div>
-          <div className="flex flex-row gap-4">
-            {finishedGames.map(({ game }: UserGame) => (
-              <div key={game.id}>
-                <button
-                  onClick={() => {
-                    setCurrentGame(game)
-                    setOpen(true)
+            <div
+              className={`min-w-[1440px] h-[300px] ${
+                finishedGame.length > 0
+                  ? 'flex flex-row gap-4'
+                  : 'flex flex-row gap-4 justify-center items-center'
+              }`}
+            >
+              {finishedGame.length > 0 ? (
+                finishedGames.map(({ game }: UserGame) => (
+                  <div key={game.id}>
+                    <button
+                      onClick={() => {
+                        setCurrentGame(game)
+                        setOpen(true)
+                      }}
+                    >
+                      <img
+                        className="max-w-56 max-h-80"
+                        src={game.gameBanner}
+                        alt=""
+                      />
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <h2 className="text-black font-bold text-3xl">
+                  Sem jogos adicionados. Procure jogos{' '}
+                  <Link to="/games" className="text-red-500">
+                    aqui.
+                  </Link>
+                </h2>
+              )}
+              <div>
+                <UserGameModal
+                  open={open}
+                  onOpenChange={open => {
+                    setOpen(open)
                   }}
                 >
-                  <img src={game.gameBanner} alt="" />
-                </button>
-              </div>
-            ))}
-            <div>
-              <UserGameModal
-                open={open}
-                onOpenChange={open => {
-                  setOpen(open)
-                }}
-              >
-                <UserGamesForm
-                  game={currentGame}
-                  afterSave={() => {
-                    setOpen(false)
-                  }}
-                  remove={removeGame}
-                  update={updateGame}
-                />
-              </UserGameModal>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col mt-10 mx-52 bg-[#272932] p-8">
-          <div className="flex flex-row gap-2  justify-between">
-            <div className="flex flex-row gap-2 items-center jus mb-2">
-              <h1 className="text-3xl font-bold text-white">Playing</h1>
-              <div className="flex flex-row items-center gap-1">
-                <PiGameControllerBold className="size-6 text-white" />
-                <p className="text-base text-white font-bold">
-                  {playingGame?.length}
-                </p>
+                  <UserGamesForm
+                    game={currentGame}
+                    afterSave={() => {
+                      setOpen(false)
+                    }}
+                    remove={removeGame}
+                    update={updateGame}
+                  />
+                </UserGameModal>
               </div>
             </div>
-            <div>
-              <Link
-                to="/userLibrary/playingGames"
-                className="text-[#8F8F8F]"
-                state={playingGame}
-              >
-                Mostrar tudo
-              </Link>
-            </div>
           </div>
-          <div className="flex flex-row gap-4">
-            {playingGames.map(({ game }: UserGame) => (
-              <div key={game.id}>
-                <button
-                  onClick={() => {
-                    setCurrentGame(game)
-                    setOpen(true)
+          <div className="flex flex-col mt-10 bg-[#272932] p-8">
+            <div className="flex flex-row gap-2  justify-between items-center">
+              <div className="flex flex-row gap-2 mb-2">
+                <h1 className="text-3xl font-bold text-white">Playing</h1>
+                <div className="flex flex-row items-center gap-1">
+                  <PiGameControllerBold className="size-6 text-white" />
+                  <p className="text-base text-white font-bold">
+                    {playingGames?.length}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <Link
+                  to="/userLibrary/playingGames"
+                  className="text-[#8F8F8F]"
+                  state={playingGame}
+                >
+                  Show all
+                </Link>
+              </div>
+            </div>
+            <div
+              className={`min-w-[1440px] h-[300px] ${
+                playingGame.length > 0
+                  ? 'flex flex-row gap-4'
+                  : 'flex flex-row gap-4 justify-center items-center'
+              }`}
+            >
+              {playingGame.length > 0 ? (
+                playingGames.map(({ game }: UserGame) => (
+                  <div key={game.id}>
+                    <button
+                      onClick={() => {
+                        setCurrentGame(game)
+                        setOpen(true)
+                      }}
+                    >
+                      <img
+                        className="max-w-56 max-h-80"
+                        src={game.gameBanner}
+                        alt=""
+                      />
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <h2 className="text-black font-bold text-3xl">
+                  Sem jogos adicionados. Procure jogos{' '}
+                  <Link to="/games" className="text-red-500">
+                    aqui.
+                  </Link>
+                </h2>
+              )}
+              <div>
+                <UserGameModal
+                  open={open}
+                  onOpenChange={open => {
+                    setOpen(open)
                   }}
                 >
-                  <img src={game.gameBanner} alt="" />
-                </button>
-              </div>
-            ))}
-            <div>
-              <UserGameModal
-                open={open}
-                onOpenChange={open => {
-                  setOpen(open)
-                }}
-              >
-                <UserGamesForm
-                  game={currentGame}
-                  afterSave={() => {
-                    setOpen(false)
-                  }}
-                  remove={removeGame}
-                  update={updateGame}
-                />
-              </UserGameModal>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col mt-10 mx-52 bg-[#272932] p-8 mb-8">
-          <div className="flex flex-row gap-2 justify-between">
-            <div className="flex flex-row gap-2 items-center mb-2">
-              <h1 className="text-3xl font-bold text-white">Paused</h1>
-              <div className="flex flex-row items-center gap-1">
-                <PiGameControllerBold className="size-6 text-white" />
-                <p className="text-base text-white font-bold">
-                  {pausedGame?.length}
-                </p>
+                  <UserGamesForm
+                    game={currentGame}
+                    afterSave={() => {
+                      setOpen(false)
+                    }}
+                    remove={removeGame}
+                    update={updateGame}
+                  />
+                </UserGameModal>
               </div>
             </div>
-            <div>
-              <Link
-                to="/userLibrary/playingGames"
-                className="text-[#8F8F8F]"
-                state={pausedGame}
-              >
-                Mostrar tudo
-              </Link>
-            </div>
           </div>
-          <div className="flex flex-row gap-4">
-            {pausedGames.map(({ game }: UserGame) => (
-              <div key={game.id}>
-                <button
-                  onClick={() => {
-                    setCurrentGame(game)
-                    setOpen(true)
+          <div className="flex flex-col mt-10 bg-[#272932] p-8 mb-8">
+            <div className="flex flex-row gap-2 justify-between items-center">
+              <div className="flex flex-row gap-2 mb-2">
+                <h1 className="text-3xl font-bold text-white">Paused</h1>
+                <div className="flex flex-row items-center gap-1">
+                  <PiGameControllerBold className="size-6 text-white" />
+                  <p className="text-base text-white font-bold">
+                    {pausedGame?.length}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <Link
+                  to="/userLibrary/playingGames"
+                  className="text-[#8F8F8F]"
+                  state={pausedGame}
+                >
+                  Show all
+                </Link>
+              </div>
+            </div>
+            <div
+              className={`min-w-[1440px] h-[300px] ${
+                pausedGame.length > 0
+                  ? 'flex flex-row gap-4'
+                  : 'flex flex-row gap-4 justify-center items-center'
+              }`}
+            >
+              {pausedGame.length > 0 ? (
+                pausedGames.map(({ game }: UserGame) => (
+                  <div key={game.id}>
+                    <button
+                      onClick={() => {
+                        setCurrentGame(game)
+                        setOpen(true)
+                      }}
+                    >
+                      <img
+                        className="max-w-56 max-h-80"
+                        src={game.gameBanner}
+                        alt=""
+                      />
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <h2 className="text-black font-bold text-3xl">
+                  Sem jogos adicionados. Procure jogos{' '}
+                  <Link to="/games" className="text-red-500">
+                    aqui.
+                  </Link>
+                </h2>
+              )}
+              <div>
+                <UserGameModal
+                  open={open}
+                  onOpenChange={open => {
+                    setOpen(open)
                   }}
                 >
-                  <img src={game.gameBanner} alt="" />
-                </button>
+                  <UserGamesForm
+                    game={currentGame}
+                    afterSave={() => {
+                      setOpen(false)
+                    }}
+                    remove={removeGame}
+                    update={updateGame}
+                  />
+                </UserGameModal>
               </div>
-            ))}
-            <div>
-              <UserGameModal
-                open={open}
-                onOpenChange={open => {
-                  setOpen(open)
-                }}
-              >
-                <UserGamesForm
-                  game={currentGame}
-                  afterSave={() => {
-                    setOpen(false)
-                  }}
-                  remove={removeGame}
-                  update={updateGame}
-                />
-              </UserGameModal>
             </div>
           </div>
         </div>
