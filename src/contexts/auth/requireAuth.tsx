b5a15999a98ec2819ac2d6664a5670b5807433e5
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-09-03 16:34:50 -0300

    save url before user login

--- src/contexts/auth/requireAuth.tsx
+++ src/contexts/auth/requireAuth.tsx
@@ -1,13 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AuthContext } from './authContext'
-import { Authentication } from '../../pages/authentication'
+import { Navigate, useLocation } from 'react-router-dom'
 
 export function RequireAuth({ children }: { children: JSX.Element }) {
   const auth = useContext(AuthContext)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (!auth.user) {
+      localStorage.setItem('redirectAfterLogin', location.pathname)
+    }
+  }, [auth.user, location])
 
   if (!auth.user) {
-    return <Authentication />
+    return <Navigate to="/auth" replace />
   }
-
   return children
 }
