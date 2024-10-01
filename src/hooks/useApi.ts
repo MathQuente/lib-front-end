9d0ef59097826400aa0f658158a41b209db1c688
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-01 13:43:28 -0300

    change some functions paramenter

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { jwtDecode } from 'jwt-decode'
+import { toast } from 'react-toastify'
+import { tokenProvider } from './token'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
@@ -9,18 +11,29 @@ interface JwtPayload {
   userId: string
 }
 
+const tokenService = tokenProvider()
+
 export const useApi = () => ({
   login: async (email: string, password: string) => {
-    const response = await api.post('/users/login', { email, password })
-    return response.data
+    try {
+      const response = await api.post('/users/login', { email, password })
+      return response.data
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMessage = error.response?.data?.message
+        toast.error(errorMessage)
+        return
+      }
+
+      toast.error('An unexpected error occurred. 🤯')
+    }
   },
   signup: async (email: string, password: string) => {
     const response = await api.post('/users', { email, password })
     return response.data
   },
   logout: async () => {
-    const token = localStorage.getItem('authToken')
-
+    const token = tokenService.getToken()
     try {
       await api.post(
         '/users/logout',
@@ -56,14 +69,24 @@ export const useApi = () => ({
     }
     return null
   },
-  getUserGames: async (userId: string) => {
+  getUserGames: async (
+    userId: string | null,
+    page?: number,
+    search?: string,
+    filter?: number
+  ) => {
     const token = localStorage.getItem('authToken')
     const response = await api.get(`/users/${userId}/userGames`, {
+      params: {
+        filter: filter,
+        pageIndex: page ? page - 1 : undefined,
+        query: search || undefined
+      },
       headers: { Authorization: `Bearer ${token}` }
     })
     return response.data
   },
-  getUserProfile: async (userId: string | undefined) => {
+  getUserProfile: async (userId: string | null) => {
     const token = localStorage.getItem('authToken')
     const response = await api.get(`/users/${userId}`, {
       headers: { Authorization: `Bearer ${token}` }
@@ -71,7 +94,7 @@ export const useApi = () => ({
     return response.data
   },
   getGames: async (page: number, search: string) => {
-    const response = await api.get(`/games`, {
+    const response = await api.get('/games', {
       params: {
         pageIndex: page - 1,
         query: search || undefined
@@ -86,12 +109,12 @@ export const useApi = () => ({
         Authorization: `Bearer ${token}`
       }
     })
-    return response.data
+    return response.data.UserGamesStatus
   },
   addGame: async (
     userId: string | null,
     gameId: string | undefined,
-    statusId: string | null
+    statusId: string | undefined
   ) => {
     const token = localStorage.getItem('authToken')
     const response = await api.post(
@@ -128,9 +151,9 @@ export const useApi = () => ({
     return response
   },
   getUserGamesFinished: async (
-    userId: string,
+    userId: string | null,
     page: number,
-    search: string
+    search?: string
   ) => {
     const token = localStorage.getItem('authToken')
     const response = await api.get(`/users/${userId}/userFinishedGames`, {
