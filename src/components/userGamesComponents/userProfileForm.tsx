15310017a2f88d0bd3258f8063bfe9dc35a411e4
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-10-01 14:07:56 -0300

    refactor and change style

--- src/components/userGamesComponents/userProfileForm.tsx
+++ src/components/userGamesComponents/userProfileForm.tsx
@@ -1,202 +1,269 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { FieldValues, useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import type { FieldValues } from 'react-hook-form'
+import type { z } from 'zod'
 
 import { RxCross2 } from 'react-icons/rx'
 import { TbCameraPlus } from 'react-icons/tb'
 import userProfilePictureDefault from '../../assets/Default_pfp.svg.png'
 
 import { updateProfileSchema } from '../../schemas/profileSchema'
-import { User } from '../../types'
+
 import { toast } from 'react-toastify'
 import { useApi } from '../../hooks/useApi'
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query'
+import type { UserProfileResponse } from '../../types/user'
 
 type UserGamesFormProps = {
   afterSave: () => void
 }
 
 export function UserProfileForm({ afterSave }: UserGamesFormProps) {
-  const [user, setUser] = useState<User>()
   const [profilePicturePreview, setProfilePicturePreview] = useState('')
   const [userBannerPreview, setUserBannerPreview] = useState('')
-
-  const [isLoading, setIsLoading] = useState(false)
+  const [removeUserBanner, setRemoveUserBanner] = useState(false)
 
   type profileForm = z.infer<typeof updateProfileSchema>
 
   const {
     register: registerUpdateProfile,
     setValue,
+
     handleSubmit,
-    formState: { errors: errosUpdateProfile }
+    formState: { errors: errosUpdateProfile },
   } = useForm<profileForm>({
-    resolver: zodResolver(updateProfileSchema)
+    resolver: zodResolver(updateProfileSchema),
   })
 
   const baseApi = useApi()
-
   const userId = baseApi.getUserIdFromToken()
+  const queryClient = useQueryClient()
+
+  const { data: UserProfileResponse } = useQuery<UserProfileResponse>({
+    queryKey: ['userProfile', userId],
+    queryFn: async () => baseApi.getUserProfile(userId),
+    placeholderData: keepPreviousData,
+  })
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      if (userId) {
-        const data = await baseApi.getUserProfile(userId)
-        setUser(data.user)
-        if (data.user) {
-          setValue('userName', data.user.userName)
-        }
-      }
+    if (UserProfileResponse?.user.userBanner) {
+      setRemoveUserBanner(true)
     }
-    fetchUserProfile()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userId, setValue])
+  }, [UserProfileResponse?.user.userBanner])
+
+  const { mutateAsync: updateUserProfile, isPending: isUpdatingProfile } =
+    useMutation({
+      mutationFn: async (data: {
+        userName?: string
+        profilePicture?: File
+        userBanner?: File
+      }) => {
+        let profilePictureUrl: string | undefined
+        let userBannerUrl: string | null
+
+        // Upload profile picture if provided
+        if (data.profilePicture) {
+          profilePictureUrl = await uploadImage(data.profilePicture)
+        }
+
+        if (!removeUserBanner) {
+          userBannerUrl = null
+        }
+
+        // Upload user banner if provided
+        if (data.userBanner) {
+          userBannerUrl = await uploadImage(data.userBanner)
+        }
+
+        // Call the updateUser function with the updated data
+        return new Promise(resolve =>
+          setTimeout(
+            () =>
+              resolve(
+                baseApi.updateUser(
+                  userId,
+                  data.userName,
+                  profilePictureUrl,
+                  userBannerUrl || ''
+                )
+              ),
+            1000
+          )
+        )
+      },
+
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ['userProfile', userId],
+        })
+        toast.success('User profile updated successfully 👌')
+        afterSave()
+      },
+      onError: error => {
+        toast.error(
+          `Update game error: ${error instanceof Error ? error.message : 'Unknown error'} 🤯`
+        )
+      },
+    })
 
   async function profileHandleSubmit(data: FieldValues) {
     if (!data) return
 
-    setIsLoading(true)
-
-    uploadProfilePicture(data)
-
-    uploadUserBanner(data)
-
-    updateUserName(data)
-
-    updateUserBanner()
+    updateUserProfile(data)
   }
 
-  async function uploadProfilePicture(data: FieldValues) {
-    if (!data.profilePicture) return
+  async function uploadImage(file: File): Promise<string> {
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('upload_preset', 'images_preset')
 
-    setProfilePicturePreview(URL.createObjectURL(data.profilePicture))
-
-    try {
-      const image = new FormData()
-      image.append('file', data.profilePicture)
-      image.append('cloud_name', 'dtdkzusmw')
-      image.append('upload_preset', 'images_preset')
-
-      const cloudName = 'dtdkzusmw'
-      const resourceType = 'image'
-      const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`
-
-      const res = await axios.post(api, image)
-      const { secure_url } = res.data
-      const profilePicture = secure_url
-      setIsLoading(false)
-      await toast.promise(
-        new Promise(resolve =>
-          setTimeout(
-            () =>
-              resolve(baseApi.updateUser(userId, undefined, profilePicture)),
-            1000
-          )
-        ),
-        {
-          pending: 'Updating...',
-          success: 'Profile picture updated with sucess.👌',
-          error: 'Update error 🤯'
-        }
-      )
-      afterSave()
-    } catch (error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-  }
-
-  async function uploadUserBanner(data: FieldValues) {
-    if (!data.userBanner) return
-
-    setUserBannerPreview(URL.createObjectURL(data.userBanner))
-
-    try {
-      const image = new FormData()
-      image.append('file', data.userBanner)
-      image.append('cloud_name', 'dtdkzusmw')
-      image.append('upload_preset', 'images_preset')
-
-      const cloudName = 'dtdkzusmw'
-      const resourceType = 'image'
-      const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`
-
-      const res = await axios.post(api, image)
-      const { secure_url } = res.data
-      const userBanner = secure_url
-      setIsLoading(false)
-      await toast.promise(
-        new Promise(resolve =>
-          setTimeout(
-            () =>
-              resolve(
-                baseApi.updateUser(userId, undefined, undefined, userBanner)
-              ),
-            1000
-          )
-        ),
-        {
-          pending: 'Updating...',
-          success: 'Banner updated with sucess.👌',
-          error: 'Update error 🤯'
-        }
-      )
-      afterSave()
-    } catch (error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-  }
-
-  async function updateUserName(data: FieldValues) {
-    if (data.userName && data.userName === user?.userName) return
-
-    try {
-      await toast.promise(
-        new Promise(resolve =>
-          setTimeout(
-            () => resolve(baseApi.updateUser(userId, data.userName)),
-            1000
-          )
-        ),
-        {
-          pending: 'Updating...',
-          success: 'Username updated with sucess.👌',
-          error: 'Update error 🤯'
-        }
-      )
-      setIsLoading(false)
-      afterSave()
-    } catch (error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-  }
-
-  async function updateUserBanner() {
-    if (
-      userBannerPreview === '' &&
-      (user?.userBanner === '' || user?.userBanner === null)
+    const response = await axios.post(
+      'https://api.cloudinary.com/v1_1/dtdkzusmw/image/upload',
+      formData
     )
-      return
 
-    await toast.promise(
-      new Promise(resolve =>
-        setTimeout(
-          () => resolve(baseApi.updateUser(userId, undefined, undefined, '')),
-          1000
-        )
-      ),
-      {
-        pending: 'Updating...',
-        success: 'Banner updated with sucess.👌',
-        error: 'Update error 🤯'
-      }
-    )
-    setIsLoading(false)
-    afterSave()
+    return response.data.secure_url
   }
+
+  // async function uploadProfilePicture(data: FieldValues) {
+  //   if (!data.profilePicture) return
+
+  //   setProfilePicturePreview(URL.createObjectURL(data.profilePicture))
+
+  //   try {
+  //     const image = new FormData()
+  //     image.append('file', data.profilePicture)
+  //     image.append('cloud_name', 'dtdkzusmw')
+  //     image.append('upload_preset', 'images_preset')
+
+  //     const cloudName = 'dtdkzusmw'
+  //     const resourceType = 'image'
+  //     const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`
+
+  //     const res = await axios.post(api, image)
+  //     const { secure_url } = res.data
+  //     const profilePicture = secure_url
+  //     setIsLoading(false)
+  //     await toast.promise(
+  //       new Promise(resolve =>
+  //         setTimeout(
+  //           () =>
+  //             resolve(baseApi.updateUser(userId, undefined, profilePicture)),
+  //           1000
+  //         )
+  //       ),
+  //       {
+  //         pending: 'Updating...',
+  //         success: 'Profile picture updated with sucess.👌',
+  //         error: 'Update error 🤯',
+  //       }
+  //     )
+  //     afterSave()
+  //   } catch (error) {
+  //     console.log(error)
+  //     setIsLoading(false)
+  //   }
+  // }
+
+  // async function uploadUserBanner(data: FieldValues) {
+  //   if (!data.userBanner) return
+
+  //   setUserBannerPreview(URL.createObjectURL(data.userBanner))
+
+  //   try {
+  //     const image = new FormData()
+  //     image.append('file', data.userBanner)
+  //     image.append('cloud_name', 'dtdkzusmw')
+  //     image.append('upload_preset', 'images_preset')
+
+  //     const cloudName = 'dtdkzusmw'
+  //     const resourceType = 'image'
+  //     const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`
+
+  //     const res = await axios.post(api, image)
+  //     const { secure_url } = res.data
+  //     const userBanner = secure_url
+  //     setIsLoading(false)
+  //     await toast.promise(
+  //       new Promise(resolve =>
+  //         setTimeout(
+  //           () =>
+  //             resolve(
+  //               baseApi.updateUser(userId, undefined, undefined, userBanner)
+  //             ),
+  //           1000
+  //         )
+  //       ),
+  //       {
+  //         pending: 'Updating...',
+  //         success: 'Banner updated with sucess.👌',
+  //         error: 'Update error 🤯',
+  //       }
+  //     )
+  //     afterSave()
+  //   } catch (error) {
+  //     console.log(error)
+  //     setIsLoading(false)
+  //   }
+  // }
+
+  // async function updateUserName(data: FieldValues) {
+  //   if (data.userName && data.userName === UserProfileResponse?.user?.userName)
+  //     return
+
+  //   try {
+  //     await toast.promise(
+  //       new Promise(resolve =>
+  //         setTimeout(
+  //           () => resolve(baseApi.updateUser(userId, data.userName)),
+  //           1000
+  //         )
+  //       ),
+  //       {
+  //         pending: 'Updating...',
+  //         success: 'Username updated with sucess.👌',
+  //         error: 'Update error 🤯',
+  //       }
+  //     )
+  //     setIsLoading(false)
+  //     afterSave()
+  //   } catch (error) {
+  //     console.log(error)
+  //     setIsLoading(false)
+  //   }
+  // }
+
+  // async function updateUserBanner() {
+  //   if (
+  //     userBannerPreview === '' &&
+  //     (UserProfileResponse?.user.userBanner === '' ||
+  //       UserProfileResponse?.user?.userBanner === null)
+  //   )
+  //     return
+
+  //   await toast.promise(
+  //     new Promise(resolve =>
+  //       setTimeout(
+  //         () => resolve(baseApi.updateUser(userId, undefined, undefined, '')),
+  //         1000
+  //       )
+  //     ),
+  //     {
+  //       pending: 'Updating...',
+  //       success: 'Banner updated with sucess.👌',
+  //       error: 'Update error 🤯',
+  //     }
+  //   )
+  //   setIsLoading(false)
+  //   afterSave()
+  // }
 
   const handleProfilePicture = (e: FieldValues) => {
     const file = e.target.files[0]
@@ -219,16 +286,14 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
       return
     }
 
-    setUser(prevUser =>
-      prevUser ? { ...prevUser, userBanner: '' } : undefined
-    )
+    setRemoveUserBanner(false)
   }
 
   return (
     <>
       <form onSubmit={handleSubmit(profileHandleSubmit)}>
         <div className="flex flex-row items-center bg-[#272932] rounded-md w-[650px] h-[260px]">
-          {!user?.userBanner && !userBannerPreview && (
+          {!userBannerPreview && !removeUserBanner && (
             <div className="flex items-center justify-center w-[650px] h-[200px]">
               <label htmlFor="userBanner">
                 <input
@@ -240,7 +305,7 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
                   {...registerUpdateProfile('userBanner', {
                     onChange: e => {
                       handleUserBanner(e)
-                    }
+                    },
                   })}
                 />
                 <div className="rounded-full bg-black opacity-60 w-10 h-10  flex items-center justify-center hover:bg-black hover:brightness-150 hover:opacity-45 hover:cursor-pointer delay-100">
@@ -249,10 +314,10 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
               </label>
             </div>
           )}
-          {(user?.userBanner || userBannerPreview) && (
+          {(userBannerPreview || removeUserBanner) && (
             <div>
               <img
-                src={userBannerPreview || user?.userBanner || ''}
+                src={userBannerPreview || UserProfileResponse?.user?.userBanner}
                 alt=""
                 className="rounded w-[650px] h-[200px] opacity-65"
               />
@@ -265,7 +330,7 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
                   {...registerUpdateProfile('userBanner', {
                     onChange: e => {
                       handleUserBanner(e)
-                    }
+                    },
                   })}
                 />
                 <div className="rounded-full bg-black opacity-60 w-10 h-10 absolute left-[330px] top-[130px] flex items-center justify-center hover:bg-black hover:brightness-150 hover:opacity-45 hover:cursor-pointer delay-100">
@@ -273,6 +338,7 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
                 </div>
               </label>
               <button
+                type="button"
                 className="rounded-full bg-black opacity-60 w-10 h-10 absolute left-[385px] top-[130px] flex items-center justify-center hover:bg-black hover:brightness-150 hover:opacity-45 hover:cursor-pointer delay-100"
                 onClick={handleRemoveBanner}
               >
@@ -285,9 +351,9 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
             <img
               src={
                 profilePicturePreview ||
-                (user?.profilePicture === null
+                (UserProfileResponse?.user?.profilePicture === null
                   ? userProfilePictureDefault
-                  : user?.profilePicture)
+                  : UserProfileResponse?.user?.profilePicture)
               }
               alt=""
               className="size-24 rounded-full"
@@ -301,7 +367,7 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
                 {...registerUpdateProfile('profilePicture', {
                   onChange: e => {
                     handleProfilePicture(e)
-                  }
+                  },
                 })}
               />
               <div className="rounded-full bg-black opacity-60 w-10 h-10 absolute left-[35px] top-[40px] flex items-center justify-center hover:bg-black hover:brightness-150 hover:opacity-45 hover:cursor-pointer delay-100">
@@ -315,8 +381,8 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
             </label>
           </div>
         </div>
-        <div className="w-full flex justify-end px-4">
-          {isLoading ? (
+        <div className="w-full flex justify-end px-4 h-10">
+          {isUpdatingProfile ? (
             'Uploading...'
           ) : (
             <button
@@ -335,7 +401,7 @@ export function UserProfileForm({ afterSave }: UserGamesFormProps) {
             <input
               type="text"
               className="bg-[#1A1C26] text-[#8F8F8F] rounded-lg block p-2.5 pl-10"
-              defaultValue={user?.userName}
+              defaultValue={UserProfileResponse?.user?.userName}
               {...registerUpdateProfile('userName')}
             />
             {errosUpdateProfile.userName && (
