47d6453965202bd06db48f0b7b90978037af3401
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-06-26 13:54:23 -0300

    style: searching for some style

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from './components/navBar'
-import { SideBar, SidebarItem } from './components/sideBar'
+import { SideBar } from './components/sideBar'
 
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import type { GamesResponse } from './types/games'
@@ -7,6 +7,7 @@ import { useApi } from './hooks/useApi'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { LayoutDashboard } from 'lucide-react'
+import dayjs from 'dayjs'
 
 export function App() {
   const [search, setSearch] = useState(() => {
@@ -30,9 +31,9 @@ export function App() {
   })
   const api = useApi()
 
-  const { data: GamesResponse } = useQuery<GamesResponse>({
+  const { data: GamesResponse } = useQuery({
     queryKey: ['games', page, search],
-    queryFn: async () => api.getGames(page, search),
+    queryFn: async () => api.test(),
     placeholderData: keepPreviousData,
   })
 
@@ -40,59 +41,164 @@ export function App() {
     return null
   }
 
+  console.log(GamesResponse)
+
   return (
     <>
       <div className="flex flex-col w-full min-h-screen bg-[#1A1C26]">
-        <Navbar />
+        {/* <Navbar /> */}
         <SideBar />
 
-        <div className="grid grid-cols-3 w-full px-4">
-          <div className="flex flex-col items-start ml-44">
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
-              <h2 className="text-white font-bold text-2xl">Last launchers</h2>
-              <div className="grid grid-cols-3 gap-2">
-                {GamesResponse.games
-                  .map(item => (
-                    <Link key={item.id} to={`/games/${item.id}`}>
-                      <img
-                        src={item.banner}
-                        className="w-32 h-40 rounded-lg"
-                        alt=""
-                      />
-                    </Link>
-                  ))
-                  .slice(12, 18)}
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-start">Recently trending</h1>
+
+          <div className="flex flex-row gap-6">
+            {GamesResponse.recentGames.map(game => (
+              <div className="flex flex-row w-full " key={game.id}>
+                {/* <Link key={game.id} to={`/games/${game.id}`}> */}
+                <img
+                  src={game.gameBanner}
+                  className="w-40 h-52 rounded-lg"
+                  alt=""
+                />
+                {/* </Link> */}
               </div>
-            </div>
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
-              <h2 className="text-white font-bold text-2xl">
-                Future launchers
-              </h2>
-              <div className="grid grid-cols-3 gap-2">
-                {GamesResponse.games
-                  .map(item => (
-                    <Link key={item.id} to={`/games/${item.id}`}>
-                      <img
-                        src={item.banner}
-                        className="w-32 h-40 rounded-lg"
-                        alt=""
-                      />
-                    </Link>
-                  ))
-                  .slice(12, 18)}
+            ))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3">
+          <div className="flex flex-col items-center justify-center flex-1">
+            <div className="w-full max-w-sm">
+              <div className="flex flex-row justify-between w-full mb-4">
+                <h1>Coming soon</h1>
+                <h1 className="text-gray-600">See more</h1>
+              </div>
+              <div className="flex flex-col gap-4">
+                {GamesResponse.futureGames.map(game => (
+                  <div className="flex flex-row w-full gap-2" key={game.id}>
+                    {/* <Link key={game.id} to={`/games/${game.id}`}> */}
+                    <img
+                      src={game.gameBanner}
+                      className="w-12 h-16 rounded-lg"
+                      alt=""
+                    />
+                    <div>
+                      <p className="text-white font-sans text-lg font-medium">
+                        {game.gameName}
+                      </p>
+                      <p className="text-gray-400">
+                        {dayjs(game.gameLaunchers[0].dateRelease).format(
+                          'MMM DD'
+                        )}
+                      </p>
+                    </div>
+                    {/* </Link> */}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col items-center">
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
-              <h2 className="text-white font-bold text-2xl">Last launchers</h2>
+          <div className="flex flex-col items-center justify-center flex-1">
+            <div className="w-full max-w-sm">
+              <div className="flex flex-row justify-between w-full mb-4">
+                <h1>Most beated</h1>
+                <h1 className="text-gray-600">See more</h1>
+              </div>
+              <div className="flex flex-col gap-4">
+                {GamesResponse.mostBeateds.map(game => (
+                  <div className="flex flex-row w-full gap-2" key={game.id}>
+                    {/* <Link key={game.id} to={`/games/${game.id}`}> */}
+                    <img
+                      src={game.gameBanner}
+                      className="w-12 h-16 rounded-lg"
+                      alt=""
+                    />
+                    <div>
+                      <p className="text-white font-sans text-lg font-medium">
+                        {game.gameName}
+                      </p>
+                      <p className="text-gray-400">
+                        {dayjs(game.gameLaunchers[0].dateRelease).format(
+                          'MMM DD'
+                        )}
+                      </p>
+                    </div>
+                    {/* </Link> */}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="col-start-2 flex flex-row items-end w-[800px] bg-[#272932] rounded-md px-8 py-6 gap-4">
+            <div className="flex flex-col items-center gap-2">
+              <h2 className="text-white font-bold text-2xl">Recent releases</h2>
+              <div className="grid grid-cols-3 gap-2">
+                {GamesResponse.recentGames.map(game => (
+                  <Link key={game.id} to={`/games/${game.id}`}>
+                    <img
+                      src={game.gameBanner}
+                      className="w-32 h-40 rounded-lg"
+                      alt=""
+                    />
+                  </Link>
+                ))}
+              </div>
+              <h2 className="text-white font-bold text-2xl">Coming soon</h2>
+              <div className="grid grid-cols-3 gap-2">
+                {GamesResponse.futureGames.map(game => (
+                  <Link key={game.id} to={`/games/${game.id}`}>
+                    <img
+                      src={game.gameBanner}
+                      className="w-32 h-40 rounded-lg"
+                      alt=""
+                    />
+                  </Link>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center gap-2 bg-[#272932]">
+              <h2 className="text-white font-bold text-2xl">Trending</h2>
+              <div className="grid grid-cols-3 gap-2">
+                {GamesResponse.gamesTrending.map(game => (
+                  <Link key={game.id} to={`/games/${game.id}`}>
+                    <img
+                      src={game.gameBanner}
+                      className="w-32 h-40 rounded-lg"
+                      alt=""
+                    />
+                  </Link>
+                ))}
+              </div>
+              <h2 className="text-white font-bold text-2xl">Most beated</h2>
+              <div className="grid grid-cols-3 gap-2">
+                {GamesResponse.mostBeateds.map(game => (
+                  <img
+                    key={game.id}
+                    src={game.gameBanner}
+                    className="w-32 h-40 rounded-lg"
+                    alt=""
+                  />
+                ))}
+              </div>
+            </div>
+          </div> */}
+
+        {/* <div className="col-start-2 flex flex-col items-center w-full">
+            <div className="flex flex-col items-center gap-2 bg-[#272932] px-10 py-4">
+              <h2 className="text-white font-bold text-2xl">
+                Most recent game releases
+              </h2>
               <div className="grid grid-cols-3 gap-2">
                 {GamesResponse.games
-                  .map(item => (
-                    <Link key={item.id} to={`/games/${item.id}`}>
+                  .map(game => (
+                    <Link key={game.id} to={`/games/${game.id}`}>
                       <img
-                        src={item.banner}
+                        src={game.gameBanner}
                         className="w-32 h-40 rounded-lg"
                         alt=""
                       />
@@ -101,7 +207,7 @@ export function App() {
                   .slice(12, 18)}
               </div>
             </div>
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
+            <div className="flex flex-col items-center gap-2 bg-[#272932] px-10 py-4">
               <h2 className="text-white font-bold text-2xl">
                 Future launchers
               </h2>
@@ -120,15 +226,15 @@ export function App() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-end mr-4">
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
+          <div className="flex flex-col items-end">
+            <div className="flex flex-col items-center gap-2 bg-[#272932] px-10 py-4">
               <h2 className="text-white font-bold text-2xl">Trending</h2>
               <div className="grid grid-cols-3 gap-2">
                 {GamesResponse.games
-                  .map(item => (
-                    <Link key={item.id} to={`/games/${item.id}`}>
+                  .map(game => (
+                    <Link key={game.id} to={`/games/${game.id}`}>
                       <img
-                        src={item.banner}
+                        src={game.gameBanner}
                         className="w-32 h-40 rounded-lg"
                         alt=""
                       />
@@ -137,14 +243,14 @@ export function App() {
                   .slice(0, 6)}
               </div>
             </div>
-            <div className="flex flex-col items-center gap-2 bg-[#272932] p-4">
+            <div className="flex flex-col items-center gap-2 bg-[#272932] px-10 py-4">
               <h2 className="text-white font-bold text-2xl">Most beated</h2>
               <div className="grid grid-cols-3 gap-2">
                 {GamesResponse.games
-                  .map(item => (
+                  .map(game => (
                     <img
-                      key={item.id}
-                      src={item.banner}
+                      key={game.id}
+                      src={game.gameBanner}
                       className="w-32 h-40 rounded-lg"
                       alt=""
                     />
@@ -152,8 +258,7 @@ export function App() {
                   .slice(6, 12)}
               </div>
             </div>
-          </div>
-        </div>
+          </div> */}
       </div>
     </>
   )
