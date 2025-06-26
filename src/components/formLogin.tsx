b56a643728db12c9e342dc3d4f45df94fa278056
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-06-26 13:23:06 -0300

    feat: implemente google 0auth2

--- src/components/formLogin.tsx
+++ src/components/formLogin.tsx
@@ -6,8 +6,11 @@ import { useContext } from 'react'
 import { AuthContext } from '../contexts/auth/authContext'
 import { useNavigate } from 'react-router-dom'
 import { RxEnvelopeClosed, RxLockOpen2 } from 'react-icons/rx'
+import logoGoogle from '../assets/Google__G__logo.svg.png'
 
 export function FormLogin() {
+  const GOOGLE_AUTH_URL = 'http://localhost:3333/auth/google'
+
   const auth = useContext(AuthContext)
   const navigate = useNavigate()
 
@@ -22,6 +25,7 @@ export function FormLogin() {
 
   async function loginHandleSubmit(data: loginForm) {
     const success = await auth.login(data.email, data.password)
+
     if (success) {
       const redirectTo = localStorage.getItem('redirectAfterLogin') || '/'
       navigate(redirectTo, { replace: true })
@@ -36,6 +40,16 @@ export function FormLogin() {
     >
       <div className="px-2 w-full flex flex-col pt-[66px] nesthub:pt-8 asus:pt-4 flex-grow">
         <div>
+          <div className="mt-6 flex justify-center">
+            <a
+              href={GOOGLE_AUTH_URL}
+              className="inline-flex items-center px-4 py-2 border rounded-md shadow-sm text-sm font-medium
+                     bg-[#1a1a1e] hover:bg-[#1a1a1e] text-white"
+            >
+              <img src={logoGoogle} alt="Google" className="h-5 w-5 mr-2" />
+              Google
+            </a>
+          </div>
           <label htmlFor="email" className="relative block text-[#ECECEC]">
             <p className="mb-4 text-base md:text-xl text-[#ECECEC] font-medium">
               Email Adress
@@ -84,12 +98,12 @@ export function FormLogin() {
               className={`bg-[#1A1C26] text-[#8F8F8F] rounded-xl block w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full text-sm md:text-xl
                p-2.5 pl-12 h-14 md:pl-14 
             ${
-              errorsLogin.email
+              errorsLogin.password
                 ? 'border-2 border-red-500 focus:outline-none'
                 : 'border-transparent'
             } 
             ${
-              !errorsLogin.email
+              !errorsLogin.password
                 ? 'focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
                 : ''
             }`}
