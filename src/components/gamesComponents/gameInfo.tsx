3c3ef64b99cf8a940d1de0db51871ee68eda8cfc
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-07-20 19:45:09 -0300

    feat: add rating average component

--- src/components/gamesComponents/gameInfo.tsx
+++ src/components/gamesComponents/gameInfo.tsx
@@ -1,17 +1,10 @@
 import type { Game } from '../../types/games'
-import Rating from '@mui/material/Rating'
-import Stack from '@mui/material/Stack'
 
 import { GameModal } from './gameModal'
 import { GameForm } from './gameForm'
 import { Link } from 'react-router-dom'
-import type { SyntheticEvent } from 'react'
-import { Skeleton, Typography } from '@mui/material'
-import { useRating } from '../../hooks/useRating'
-import { X } from 'lucide-react'
 import { useAuth } from '../../hooks/useAuth'
-import { useQuery } from '@tanstack/react-query'
-import { useApi } from '../../hooks/useApi'
+import { RatingAverage } from '../ratingAverage'
 
 interface GameInfoProps {
   game: Game
@@ -19,46 +12,7 @@ interface GameInfoProps {
 }
 
 export function GameInfo({ game, onClose }: GameInfoProps) {
-  const api = useApi()
-  const { rating, isLoading, isError, addRating, removeRating, isMutating } =
-    useRating(game.id)
-
-  const {
-    data: average,
-    isLoading: isAverageLoading,
-    isError: isAverageError,
-  } = useQuery({
-    queryFn: () => api.getAverageRating(game.id),
-    queryKey: [game.id, rating, 'averageRating'],
-    enabled: Boolean(game.id),
-    staleTime: 1000 * 60 * 5,
-  })
-
   const { user } = useAuth()
-
-  if (isLoading) return <Skeleton variant="rounded" width={200} height={40} />
-  if (isError) return <div>Erro ao carregar avaliação</div>
-
-  const handleRatingChange = async (
-    _: SyntheticEvent,
-    newValue: number | null
-  ) => {
-    if (newValue === null) return
-    await addRating(newValue)
-  }
-
-  async function handleDelete() {
-    await removeRating()
-  }
-
-  const getAverageRatingDisplay = () => {
-    if (isAverageLoading) return 'Carregando...'
-    if (isAverageError) return 'Erro ao carregar'
-    if (!average || average.average === undefined || average.average === null) {
-      return 'Sem avaliações'
-    }
-    return average.average.toFixed(1) // Display with 1 decimal place
-  }
 
   return (
     <div key={game.id} className="flex flex-col p-4">
@@ -80,46 +34,7 @@ export function GameInfo({ game, onClose }: GameInfoProps) {
 
           {user && <GameForm game={game} />}
 
-          {user && (
-            <div>
-              <Stack
-                spacing={2}
-                direction="row"
-                alignItems="center"
-                sx={{ position: 'relative' }}
-              >
-                {rating && (
-                  <button
-                    type="button"
-                    aria-label="Delete rating"
-                    onClick={handleDelete}
-                    disabled={!rating || isMutating}
-                    className="absolute -left-3 text-gray-400 hover:text-white text-sm translate-x-1"
-                    style={{}}
-                  >
-                    <X />
-                  </button>
-                )}
-
-                <Rating
-                  name="half-rating"
-                  value={rating}
-                  onChange={handleRatingChange}
-                  disabled={isMutating}
-                  precision={0.5}
-                  size="large"
-                />
-              </Stack>
-              <div className="flex flex-col">
-                <Typography>
-                  Sua avaliação: {rating !== null ? rating : 'Nenhuma'}
-                </Typography>
-                <Typography>
-                  Avaliação geral: {getAverageRatingDisplay()}
-                </Typography>
-              </div>
-            </div>
-          )}
+          {user && <RatingAverage gameId={game.id} />}
 
           {!user && (
             <div className="flex gap-1">
