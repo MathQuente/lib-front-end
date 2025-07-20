62bb4a78d648a48fc7633ac38b03889bc9a9982e
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-07-20 19:41:19 -0300

    style: adjust larger size

--- src/components/gamesComponents/gameCard.tsx
+++ src/components/gamesComponents/gameCard.tsx
@@ -30,7 +30,7 @@ export function GameCard({
     small: 'w-12 h-16 rounded-lg',
     medium:
       'rounded-lg object-cover w-32 h-16 sm:w-36 md:w-44 md:h-56 sm:h-40 lg:min-w-28 lg:h-44 xl:w-48 xl:h-52 2xl:w-48 2xl:min-h-64',
-    larger: 'w-40 h-52 rounded-lg'
+    larger: 'w-40 h-48 lg:w-44 lg:h-52 rounded-lg'
   }
   return (
     <>
