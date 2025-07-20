daaeb83668be1c0a557aa95c89536915cbc0f75f
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-07-20 19:44:39 -0300

    style: fixing syles

--- src/components/sidebarItem.tsx
+++ src/components/sidebarItem.tsx
@@ -16,17 +16,17 @@ export function SideBarItem({
 }: SideBarItemProps) {
   return (
     <li
-      className={`relative flex items-center jus py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${
+      className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${
         active
           ? 'bg-gradient-to-tr from-indigo-200 to-indigo-100 text-[#7A38CA]'
           : 'hover:bg-indigo-50 text-gray-600'
       }`}
     >
-      <Link to={to} className="flex items-center gap-2 w-full">
+      <Link to={to} className="flex items-center w-full">
         {icon}
         <span
           className={`overflow-hidden transition-all ${
-            expanded ? 'w-52' : 'w-0'
+            expanded ? 'w-52 ml-3' : 'w-0'
           }`}
         >
           {text}
@@ -38,12 +38,12 @@ export function SideBarItem({
             }`}
           />
         )}
-        {!expanded && (
-          <div className="absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-[#7A38CA] text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0">
-            {text}
-          </div>
-        )}
       </Link>
+      {!expanded && (
+        <div className="absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-[#7A38CA] text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0">
+          {text}
+        </div>
+      )}
     </li>
   )
 }
