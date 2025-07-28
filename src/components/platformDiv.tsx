04a8764afd5be738b6bef995daa9f2026f644a29
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2025-07-28 18:46:23 -0300

    style: add inline-flex

--- src/components/platformDiv.tsx
+++ src/components/platformDiv.tsx
@@ -32,7 +32,7 @@ export function PlatformDiv({ platformName }: PlaformDivProps) {
   const PlatformIcon = getPlatformIcon(platformName)
 
   return (
-    <div className="px-3 py-1 bg-gray-700/50 rounded-full text-sm flex items-center gap-1">
+    <div className="px-3 py-1 bg-gray-700/50 rounded-full text-sm inline-flex items-center justify-center gap-1">
       <PlatformIcon className="text-gray-300 w-5 h-5 font-normal" />
       <p className="text-gray-300 md:text-sm lg:text-base font-normal">
         {platformName}
